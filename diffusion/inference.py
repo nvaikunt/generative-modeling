851d05eb46f0a4b1f0cbc0d5652dcfbc99700bc5
nvaikunt <nvaikunt@andrew.cmu.edu>
2023-03-18 16:09:11 -0400

    Diffusion First Cut

--- diffusion/inference.py
+++ diffusion/inference.py
@@ -11,6 +11,7 @@ from cleanfid import fid as cleanfid
 def get_fid(gen, dataset_name, dataset_resolution, z_dimension, batch_size, num_gen):
     # TODO 3.3: Write a function that samples images from the diffusion model given z
     # NOTE: the output must be [0, 255]
+    gen_fn = gen.sample_given_z
     score = cleanfid.compute_fid(
         gen=gen_fn,
         dataset_name=dataset_name,
